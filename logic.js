b5b9da3f4c3253990e8232531a2a3fb97a23b620
Amarius1 <amarius2000@gmail.com>
2018-10-27 23:19:25 +0300

    More responsive stuff

--- logic.js
+++ logic.js
@@ -7,17 +7,17 @@ document.addEventListener("DOMContentLoaded", function(){
 });
 AOS.init()
 $(function() {
-    var nav = $("#nav");
+    var a = $("#title, #title-icon");
     $(window).scroll(function() {    
         var scroll = $(window).scrollTop();
     
-        if (scroll >= 60) {
-			nav.removeClass("z-depth-0");
-			nav.addClass("z-depth-1");
+        if (scroll >= 40) {
+			a.addClass("gone");
+			i.addClass("gone");
 			 
         } else {
-            nav.removeClass("z-depth-1");
-			nav.addClass("z-depth-0");
+            a.removeClass("gone");
+			i.removeClass("gone");
         }
     });
 });    
@@ -26,14 +26,16 @@ jQuery(document).ready(function($) {
   var alterClass = function() {
     var ww = document.body.clientWidth;
     if (ww < 1200) {
-		$('#list').removeClass('row','container');
+		$('#list').removeClass('row');
 		$('#list-child').removeClass('col','s1','m3');
+		$('#card,#card2,#card3,#card4').addClass('container');
 		$('#title').addClass('phone-title-smol');
 		$('#title-icon').addClass('phone-title-label-smol');
 		document.getElementById("title-icon").style.fontSize = "x-large";
     } else if (ww >= 1201) {
-		$('#list').addClass('row','container');
+		$('#list').addClass('row');
 		$('#list-child').addClass('col','s1','m3');
+		$('#card,#card2,#card3,#card4').removeClass('container');
 		$('#title').removeClass('phone-title-smol');
 		$('#title-icon').removeClass('phone-title-label-smol');
 		document.getElementById("title-icon").style.fontSize= "xx-large";
