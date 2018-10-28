9d37380880332e8b5a7458df6bd449dd22002203
Amarius1 <amarius2000@gmail.com>
2018-10-28 17:17:25 +0200

    Add files via upload

--- logic.js
+++ logic.js
@@ -21,29 +21,3 @@ $(function() {
         }
     });
 });    
-
-jQuery(document).ready(function($) {
-  var alterClass = function() {
-    var ww = document.body.clientWidth;
-    if (ww < 1200) {
-		$('#list').removeClass('row');
-		$('#list-child').removeClass('col','s1','m3');
-		$('#card,#card2,#card3,#card4').addClass('container');
-		$('#title').addClass('phone-title-smol');
-		$('#title-icon').addClass('phone-title-label-smol');
-		document.getElementById("title-icon").style.fontSize = "x-large";
-    } else if (ww >= 1201) {
-		$('#list').addClass('row');
-		$('#list-child').addClass('col','s1','m3');
-		$('#card,#card2,#card3,#card4').removeClass('container');
-		$('#title').removeClass('phone-title-smol');
-		$('#title-icon').removeClass('phone-title-label-smol');
-		document.getElementById("title-icon").style.fontSize= "xx-large";
-    };
-  };
-  $(window).resize(function(){
-    alterClass();
-  });
-  //Fire it when the page first loads:
-  alterClass();
-});
